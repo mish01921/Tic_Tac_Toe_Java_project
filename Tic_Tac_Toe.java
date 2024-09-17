2fb5fe667e03791409e6f4469e28f71f3a4d9f9f
Misha Baghdasaryan <misho191524@gmail.com>
2024-09-17 20:24:03 +0300

    finish

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -10,58 +10,66 @@ public class Tic_Tac_Toe {
 	static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
-		
-		char[][] gameBoard = {
-				{' ', '|', ' ', '|', ' '},
-				{'-', '+', '-', '+', '-'},
-				{' ', '|', ' ', '|', ' '},
-				{'-', '+', '-', '+', '-'},
-				{' ', '|', ' ', '|', ' '}
-				};
 
-		printGameBoard(gameBoard);
+		Scanner scanner = new Scanner(System.in);
 
-		while (true) {
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("Enter your placement (1-9)");
-			int playerPosition = scanner.nextInt();
-			while (playerPositions.contains(playerPosition) || cpuPositions.contains(playerPositions)) {
-				System.out.println("Position taken! Enter a correct Position");
-				playerPosition = scanner.nextInt();
-				
-			}
+		while (true) { // Առաջին հիմնական ցիկլը
+			char[][] gameBoard = {
+					{' ', '|', ' ', '|', ' '},
+					{'-', '+', '-', '+', '-'},
+					{' ', '|', ' ', '|', ' '},
+					{'-', '+', '-', '+', '-'},
+					{' ', '|', ' ', '|', ' '}
+			};
 
-			placePiece(gameBoard, playerPosition, "player");
-			String result = checkWinner();
-			if(result.length() > 0) {
-				System.out.println(result);
-				break;
-			}
-			
-			
-			Random random = new Random();
-			int cpuPosition = random.nextInt(9) + 1;
-			
-			while (playerPositions.contains(cpuPosition) || cpuPositions.contains(cpuPosition)) {
-				cpuPosition = random.nextInt(9) + 1;
-				
-			}
+			playerPositions.clear();
+			cpuPositions.clear();
 
-			placePiece(gameBoard, cpuPosition, "cpu");
 			printGameBoard(gameBoard);
-			 
-			result = checkWinner();
-			if(result.length() > 0) {
-				System.out.println(result);
-				break;
-			}
-			
-			
-	}
-			
-}
 
-	
+			while (true) {
+				System.out.println("Enter your placement (1-9):");
+				int playerPosition = scanner.nextInt();
+				while (playerPositions.contains(playerPosition) || cpuPositions.contains(playerPosition)) {
+					System.out.println("Position taken! Enter a correct Position");
+					playerPosition = scanner.nextInt();
+				}
+
+				placePiece(gameBoard, playerPosition, "player");
+				String result = checkWinner();
+				if (result.length() > 0) {
+					System.out.println(result);
+					break;
+				}
+
+				Random random = new Random();
+				int cpuPosition = random.nextInt(9) + 1;
+
+				while (playerPositions.contains(cpuPosition) || cpuPositions.contains(cpuPosition)) {
+					cpuPosition = random.nextInt(9) + 1;
+				}
+
+				placePiece(gameBoard, cpuPosition, "cpu");
+				printGameBoard(gameBoard);
+
+				result = checkWinner();
+				if (result.length() > 0) {
+					System.out.println(result);
+					break;
+				}
+			}
+
+			// Հարցում օգտատիրոջը՝ արդյոք ցանկանում է խաղալ կրկին
+			System.out.println("Do you want to play again? (x/n)");
+			char response = scanner.next().charAt(0);
+			if (response == 'n' || response == 'N') {
+				System.out.println("Thanks for playing!");
+				break; // Կրկին չխաղալու դեպքում դուրս է գալիս հիմնական ցիկլից
+			}
+		}
+
+		scanner.close(); // Ապահով փակել սկաները
+	}
 
 	public static void printGameBoard(char[][] gameBoard) {
 		for (char[] row : gameBoard) {
@@ -83,65 +91,61 @@ public class Tic_Tac_Toe {
 		}
 
 		switch (position) {
-		case 1:
-			gameBoard[0][0] = symbol;
-			break;
-		case 2:
-			gameBoard[0][2] = symbol;
-			break;
-		case 3:
-			gameBoard[0][4] = symbol;
-			break;
-		case 4:
-			gameBoard[2][0] = symbol;
-			break;
-		case 5:
-			gameBoard[2][2] = symbol;
-			break;
-		case 6:
-			gameBoard[2][4] = symbol;
-			break;
-		case 7:
-			gameBoard[4][0] = symbol;
-			break;
-		case 8:
-			gameBoard[4][2] = symbol;
-			break;
-		case 9:
-			gameBoard[4][4] = symbol;
-			break;
-		default:
-			break;
+			case 1:
+				gameBoard[0][0] = symbol;
+				break;
+			case 2:
+				gameBoard[0][2] = symbol;
+				break;
+			case 3:
+				gameBoard[0][4] = symbol;
+				break;
+			case 4:
+				gameBoard[2][0] = symbol;
+				break;
+			case 5:
+				gameBoard[2][2] = symbol;
+				break;
+			case 6:
+				gameBoard[2][4] = symbol;
+				break;
+			case 7:
+				gameBoard[4][0] = symbol;
+				break;
+			case 8:
+				gameBoard[4][2] = symbol;
+				break;
+			case 9:
+				gameBoard[4][4] = symbol;
+				break;
+			default:
+				break;
 		}
 	}
 
 	public static String checkWinner() {
+		List<Integer> topRow = Arrays.asList(1, 2, 3);
+		List<Integer> midRow = Arrays.asList(4, 5, 6);
+		List<Integer> botRow = Arrays.asList(7, 8, 9);
+		List<Integer> leftCol = Arrays.asList(1, 4, 7);
+		List<Integer> midCol = Arrays.asList(2, 5, 8);
+		List<Integer> rightCol = Arrays.asList(3, 6, 9);
+		List<Integer> cross1 = Arrays.asList(1, 5, 9);
+		List<Integer> cross2 = Arrays.asList(7, 5, 3);
 
-		List topRow = Arrays.asList(1, 2, 3);
-		List midRow = Arrays.asList(4, 5, 6);
-		List botRow = Arrays.asList(7, 8, 9);
-
-		List letfCoL = Arrays.asList(1, 4, 7);
-		List midCol = Arrays.asList(2, 5, 8);
-		List rightCol = Arrays.asList(3, 6, 9);
-
-		List leftToRight = Arrays.asList(1, 5, 9);
-		List rightToLeft = Arrays.asList(7, 5, 3);
-
-		List<List> winning = new ArrayList<List>();
+		List<List<Integer>> winning = new ArrayList<List<Integer>>();
 		winning.add(topRow);
 		winning.add(midRow);
 		winning.add(botRow);
-		winning.add(letfCoL);
+		winning.add(leftCol);
 		winning.add(midCol);
 		winning.add(rightCol);
-		winning.add(leftToRight);
-		winning.add(rightToLeft);
+		winning.add(cross1);
+		winning.add(cross2);
 
-		for (List l : winning) {
-
+		for (List<Integer> l : winning) {
 			if (playerPositions.containsAll(l)) {
-				return "Congatulations you won";
+				return "Congratulations, you won!";
 			} else if (cpuPositions.containsAll(l)) {
 				return "CPU wins! You lose :(";
 			} else if (playerPositions.size() + cpuPositions.size() == 9) {
@@ -150,6 +154,5 @@ public class Tic_Tac_Toe {
 		}
 
 		return "";
-
 	}
 }
