2871d35ed5b26d54a9ad1909b596678d5fc27f27
Misha Baghdasaryan <misho191524@gmail.com>
2024-09-17 16:50:58 +0300

    i added any code

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,5 +1,118 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Tic_Tac_Toe {
 	public static void main(String[] args) {
-		System.out.println("My First java Game");
+
+		char[][] gameBoard = {
+				{' ', '|', ' ', '|', ' '},
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '},
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '}
+				};
+		
+		
+		printGameBoard(gameBoard);
+		
+		
+		
+		while(true) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter your placement (1-9)");
+			int playerPosition = scanner.nextInt();
+
+			
+			placePiece(gameBoard,playerPosition,"player");
+			
+			Random random  = new Random();
+			int cpuPosition = random.nextInt(9) + 1;
+			
+			
+			placePiece(gameBoard, cpuPosition, "cpu");
+			printGameBoard(gameBoard);
+		}
+		
+		
 	}
+	
+	public static void printGameBoard(char[][] gameBoard) {
+		for (char[] row : gameBoard) {
+			for (char c : row) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+	}
+		public static void placePiece(char[][] gameBoard, int position, String user) {
+			char symbol = ' ';
+			if (user.equals("player")) {
+				symbol = 'X';
+			}else if (user.equals("cpu")) {
+				symbol = 'O';
+			}
+			
+			
+			
+			switch (position) {
+			case 1:
+				gameBoard[0][0] = symbol;
+				break;
+			case 2:
+				gameBoard[0][2] = symbol;
+				break;
+			case 3:
+				gameBoard[0][4] = symbol;
+				break;
+			case 4:
+				gameBoard[2][0] = symbol;
+				break;
+			case 5:
+				gameBoard[2][2] = symbol;
+				break;
+			case 6:
+				gameBoard[2][4] = symbol;
+				break;
+			case 7:
+				gameBoard[4][0] = 'X';
+				break;
+			case 8:
+				gameBoard[4][2] = symbol;
+				break;
+			case 9:
+				gameBoard[4][4] = symbol;
+				break;
+			default:
+				break;
+			}
+			
+			//System.out.println(position);
+			//printGameBoard(gameBoard);
+		}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
